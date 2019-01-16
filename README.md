ac84e61ce1e4e2e57b47d1e2f63214f73104e515
Jefersson Nathan <malukenho.dev@gmail.com>
2019-01-16 23:08:12 +0100

    Copy result automatically to the clipboard
    Several changes were made on code and on UI elements. The second textbox
    were totally removed and instead we copy the result to the clipboard of
    the user when the conversion succeds.
    
    The new shortcut to invoke the screen is `Command + Option + S`, not
    sure we will maintain it, but as the user can freely change it from the
    IDE config, I wouldn't botter that much.
    
    Acked-by: Airton Zanon <airtonzanon@gmail.com>
    Acked-by: Alexandre Eher <alexandre.eher@werkspot.nl>
    Signed-off-by: Jefersson Nathan <malukenho.dev@gmail.com>

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
-Salesforce ID Generator
-================================
+🌭 Salesforce ID Generator
+==========================
 
-It is a simple Salesforce plugin to generate 18 digits IDs out of 15 digits ID.
+![Conversion Example](./screen.png)
 
-You can install it via IntelliJ Plugin Market
+This simple plugin generates a 18 digits ID out of a 15 digits ID for Salesforce.
+
+To use it, just press `⌘ + ⌥ + s`.
+
+When loaded it will try to check on your clipboard for a 15 length string, if
+it finds it then an automatically conversion is done and copied to your clipboard,
+otherwise, you can fill in the _15.len() ID_ field manually.
+
+#### Author
+
+- Jefersson Nathan - [@malukenho](http://github.com/malukenho)
+- Airton Zanon - [@airtonzanon](http://github.com/airtonzanon)
+- Alexandre Eher - [@EHER](http://github.com/EHER)
--- src/com/malukenho/saleforceid/ShowDialog.java
+++ src/com/malukenho/saleforceid/ShowDialog.java
@@ -2,14 +2,21 @@ package com.malukenho.saleforceid;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.application.ex.ClipboardUtil;
+import com.intellij.openapi.ide.CopyPasteManager;
 import com.intellij.openapi.wm.IdeFrame;
 import com.intellij.openapi.wm.WindowManager;
+import com.intellij.ui.Colors;
+import com.intellij.ui.components.JBLabel;
+import com.intellij.ui.components.JBPanel;
+import com.intellij.util.ui.JBUI;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 import java.awt.*;
+import java.awt.datatransfer.StringSelection;
 
 public class ShowDialog extends AnAction {
     @Override
@@ -24,53 +31,89 @@ public class ShowDialog extends AnAction {
         );
 
         JDialog dialog = new JDialog();
+        dialog.setResizable(false);
         dialog.setContentPane(mount());
-        dialog.setSize(350, 100);
+        dialog.setSize(350, 200);
         dialog.setLocation(middle);
         dialog.setVisible(true);
     }
 
     private static JPanel mount() {
+
+        String textInClipboard = ClipboardUtil.getTextInClipboard();
+        String clipboardResult = ConvertSalesForceId.convertTo18CharId(textInClipboard);
+
         JPanel container = new JPanel(new BorderLayout());
+        container.setBorder(JBUI.Borders.empty(15));
 
         JPanel mainPanel = new JPanel();
         mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.PAGE_AXIS));
 
         JPanel line1 = new JPanel();
         line1.setLayout(new GridLayout(1, 2));
-        line1.add(new JLabel("15 Length ID"));
+        line1.add(new JLabel("15.len() ID"));
         final JTextField encodedField = new JTextField(10);
+        if (clipboardResult != null) {
+            encodedField.setText(textInClipboard);
+        }
         line1.add(encodedField);
 
-        JPanel line2 = new JPanel();
-        line2.setLayout(new GridLayout(1, 2));
-        line2.add(new JLabel("Translated"));
-        final JTextField decodedField = new JTextField(10);
-        line2.add(decodedField);
+        JPanel line2 = new JBPanel();
+        JBLabel decodedLabel = new JBLabel("NOPE", SwingConstants.CENTER);
+        decodedLabel.setForeground(Colors.DARK_RED);
+        decodedLabel.setBorder(JBUI.Borders.emptyTop(20));
+        decodedLabel.setFont(new Font(decodedLabel.getName(), Font.PLAIN, 25));
 
-        // @TODO remove duplication
+        JBLabel decodedLabel2 = new JBLabel(" ", SwingConstants.CENTER);
+        decodedLabel2.setText(" ");
+
+        calculateID(textInClipboard, decodedLabel, decodedLabel2);
+
+        line2.add(decodedLabel);
+
+        JPanel line3 = new JBPanel();
+        line3.add(decodedLabel2);
+
         encodedField.getDocument().addDocumentListener(new DocumentListener() {
             @Override
             public void insertUpdate(DocumentEvent e) {
-                decodedField.setText(ConvertSalesForceId.convertTo18CharId(encodedField.getText()));
+                calculateID(encodedField.getText(), decodedLabel, decodedLabel2);
             }
 
             @Override
             public void removeUpdate(DocumentEvent e) {
-                decodedField.setText(ConvertSalesForceId.convertTo18CharId(encodedField.getText()));
+                calculateID(encodedField.getText(), decodedLabel, decodedLabel2);
             }
 
             @Override
             public void changedUpdate(DocumentEvent e) {
-                decodedField.setText(ConvertSalesForceId.convertTo18CharId(encodedField.getText()));
+                calculateID(encodedField.getText(), decodedLabel, decodedLabel2);
             }
         });
 
         mainPanel.add(line1);
         mainPanel.add(line2);
+        mainPanel.add(line3);
 
         container.add(mainPanel, BorderLayout.NORTH);
 
         return container;
+    }
+
+    private static void calculateID(String encodedField, JLabel decodedLabel, JLabel decodedLabel2) {
+        String encoded = ConvertSalesForceId.convertTo18CharId(encodedField);
+
+        if (encoded == null) {
+            decodedLabel.setText("NOPE");
+            decodedLabel.setForeground(Colors.DARK_RED);
+            decodedLabel2.setText("No valid ID were found");
+            return;
+        }
+        CopyPasteManager copyPasteManager = CopyPasteManager.getInstance();
+        copyPasteManager.setContents(new StringSelection(encoded));
+
+        decodedLabel.setText(ConvertSalesForceId.convertTo18CharId(encodedField));
+        decodedLabel.setForeground(Colors.DARK_GREEN);
+        decodedLabel2.setText("Copied to your clipboard!");
     }
 }
